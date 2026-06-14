e3ba4300b704594a71c0d79e4f62f6b05601f9ae
Muhammad Rakha Akbar <rakhaakbar522@gmail.com>
2026-06-14 10:22:08 +0700

    revert: restore data to Jun 12 state

--- src/routes/adminTraineesRoutes.js
+++ src/routes/adminTraineesRoutes.js
@@ -80,16 +80,19 @@ router.post('/', async (req, res) => {
     const insertQuery = `
       INSERT INTO dashboard_trainne (
         id, trainee_name, status, program, class, level, membership_expiry,
-        last_speaking_project, progress_ke_next_level, weekly_report,
-        referral_code, gold_rank, progress_video, password, plain_password,
-        phone, profile_picture, tanggal_lahir, cabang, house_sml, junior_youth,
-        gender, school, first_enroll, house_role, class_branch, newest_grade,
-        screening_test, last_life_project_date,
-        last_life_project, tautan_tambahan
+        last_speaking_project, progress_ke_next_level, highlight_terbaru,
+        pengumuman, weekly_report, quarterly_report, referral_code, gold_rank,
+        progress_video, laporan_sebelumnya, laporan_quarter_sebelumnya,
+        completed_speaking_project, password, plain_password, phone, profile_picture, tanggal_lahir,
+        cabang, house_sml, total_gold_periode, junior_youth,
+        rank_id_junior, rank_id_youth,
+        rank_id_junior_timor, rank_id_youth_timor,
+        rank_id_junior_tritura, rank_id_youth_tritura,
+        rank_id_junior_cemara, rank_id_youth_cemara
       ) VALUES (
         $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16,
         $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30,
-        $31
+        $31, $32, $33, $34, $35, $36
       ) RETURNING *;
     `;
 
@@ -102,14 +105,16 @@ router.post('/', async (req, res) => {
       data.id, data.trainee_name, data.status || 'Active', data.program || null, classValue,
       data.level || null, data.membership_expiry || null,
       data.last_speaking_project || null, data.progress_ke_next_level || null,
-      data.weekly_report || null, data.referral_code || null, data.gold_rank || null,
-      data.progress_video || null, hashedPassword, plainPassword, data.phone || null,
-      data.profile_picture || null, data.tanggal_lahir || null, data.cabang || null,
-      data.house_sml || null, data.junior_youth || null, data.gender || null,
-      data.school || null, data.first_enroll || null, data.house_role || null,
-      data.class_branch || null, data.newest_grade || null, data.screening_test || null,
-      data.last_life_project_date || null, data.last_life_project || null,
-      data.tautan_tambahan || null
+      data.highlight_terbaru || null, data.pengumuman || null, data.weekly_report || null,
+      data.quarterly_report || null, data.referral_code || null, data.gold_rank || null,
+      data.progress_video || null, data.laporan_sebelumnya || null,
+      data.laporan_quarter_sebelumnya || null, data.completed_speaking_project || null,
+      hashedPassword, plainPassword, data.phone || null, data.profile_picture || null, data.tanggal_lahir || null,
+      data.cabang || null, data.house_sml || null, data.total_gold_periode || null, data.junior_youth || null,
+      data.rank_id_junior || null, data.rank_id_youth || null,
+      data.rank_id_junior_timor || null, data.rank_id_youth_timor || null,
+      data.rank_id_junior_tritura || null, data.rank_id_youth_tritura || null,
+      data.rank_id_junior_cemara || null, data.rank_id_youth_cemara || null
     ];
 
     const result = await db.query(insertQuery, values);
@@ -153,13 +158,17 @@ router.put('/:id', async (req, res) => {
       UPDATE dashboard_trainne SET
         trainee_name = $1, status = $2, program = $3, class = $4, level = $5,
         membership_expiry = $6, last_speaking_project = $7,
-        progress_ke_next_level = $8, weekly_report = $9, referral_code = $10,
-        gold_rank = $11, progress_video = $12, password = $13, plain_password = $14,
-        phone = $15, profile_picture = $16, tanggal_lahir = $17, cabang = $18,
-        house_sml = $19, junior_youth = $20, gender = $21, school = $22,
-        first_enroll = $23, house_role = $24, class_branch = $25, newest_grade = $26,
-        screening_test = $27, last_life_project_date = $28, last_life_project = $29, tautan_tambahan = $30
-      WHERE id = $31 RETURNING *;
+        progress_ke_next_level = $8, highlight_terbaru = $9, pengumuman = $10,
+        weekly_report = $11, quarterly_report = $12, referral_code = $13, gold_rank = $14,
+        progress_video = $15, laporan_sebelumnya = $16, laporan_quarter_sebelumnya = $17,
+        completed_speaking_project = $18, password = $19, plain_password = $20, phone = $21,
+        profile_picture = $22, tanggal_lahir = $23,
+        cabang = $24, house_sml = $25, total_gold_periode = $26, junior_youth = $27,
+        rank_id_junior = $28, rank_id_youth = $29,
+        rank_id_junior_timor = $30, rank_id_youth_timor = $31,
+        rank_id_junior_tritura = $32, rank_id_youth_tritura = $33,
+        rank_id_junior_cemara = $34, rank_id_youth_cemara = $35
+      WHERE id = $36 RETURNING *;
     `;
 
     let classValue = data.class !== undefined ? data.class : current.class;
@@ -176,10 +185,16 @@ router.put('/:id', async (req, res) => {
       data.membership_expiry !== undefined ? data.membership_expiry : current.membership_expiry,
       data.last_speaking_project !== undefined ? data.last_speaking_project : current.last_speaking_project,
       data.progress_ke_next_level !== undefined ? data.progress_ke_next_level : current.progress_ke_next_level,
+      data.highlight_terbaru !== undefined ? data.highlight_terbaru : current.highlight_terbaru,
+      data.pengumuman !== undefined ? data.pengumuman : current.pengumuman,
       data.weekly_report !== undefined ? data.weekly_report : current.weekly_report,
+      data.quarterly_report !== undefined ? data.quarterly_report : current.quarterly_report,
       data.referral_code !== undefined ? data.referral_code : current.referral_code,
       data.gold_rank !== undefined ? data.gold_rank : current.gold_rank,
       data.progress_video !== undefined ? data.progress_video : current.progress_video,
+      data.laporan_sebelumnya !== undefined ? data.laporan_sebelumnya : current.laporan_sebelumnya,
+      data.laporan_quarter_sebelumnya !== undefined ? data.laporan_quarter_sebelumnya : current.laporan_quarter_sebelumnya,
+      data.completed_speaking_project !== undefined ? data.completed_speaking_project : current.completed_speaking_project,
       passwordValue,
       plainPasswordValue,
       data.phone !== undefined ? data.phone : current.phone,
@@ -187,17 +202,16 @@ router.put('/:id', async (req, res) => {
       data.tanggal_lahir !== undefined ? data.tanggal_lahir : current.tanggal_lahir, 
       data.cabang !== undefined ? data.cabang : current.cabang,
       data.house_sml !== undefined ? data.house_sml : current.house_sml,
+      data.total_gold_periode !== undefined ? data.total_gold_periode : current.total_gold_periode,
       data.junior_youth !== undefined ? data.junior_youth : current.junior_youth,
-      data.gender !== undefined ? data.gender : current.gender,
-      data.school !== undefined ? data.school : current.school,
-      data.first_enroll !== undefined ? data.first_enroll : current.first_enroll,
-      data.house_role !== undefined ? data.house_role : current.house_role,
-      data.class_branch !== undefined ? data.class_branch : current.class_branch,
-      data.newest_grade !== undefined ? data.newest_grade : current.newest_grade,
-      data.screening_test !== undefined ? data.screening_test : current.screening_test,
-      data.last_life_project_date !== undefined ? data.last_life_project_date : current.last_life_project_date,
-      data.last_life_project !== undefined ? data.last_life_project : current.last_life_project,
-      data.tautan_tambahan !== undefined ? data.tautan_tambahan : current.tautan_tambahan,
+      data.rank_id_junior !== undefined ? data.rank_id_junior : current.rank_id_junior,
+      data.rank_id_youth !== undefined ? data.rank_id_youth : current.rank_id_youth,
+      data.rank_id_junior_timor !== undefined ? data.rank_id_junior_timor : current.rank_id_junior_timor,
+      data.rank_id_youth_timor !== undefined ? data.rank_id_youth_timor : current.rank_id_youth_timor,
+      data.rank_id_junior_tritura !== undefined ? data.rank_id_junior_tritura : current.rank_id_junior_tritura,
+      data.rank_id_youth_tritura !== undefined ? data.rank_id_youth_tritura : current.rank_id_youth_tritura,
+      data.rank_id_junior_cemara !== undefined ? data.rank_id_junior_cemara : current.rank_id_junior_cemara,
+      data.rank_id_youth_cemara !== undefined ? data.rank_id_youth_cemara : current.rank_id_youth_cemara,
       id
     ];
 
