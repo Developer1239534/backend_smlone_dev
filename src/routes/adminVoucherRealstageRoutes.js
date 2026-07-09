9a34b7a81adfa418ffc2a6a03081f79062beb591
Muhammad Rakha Akbar <rakhaakbar522@gmail.com>
2026-07-09 11:26:22 +0700

    fix: correct database import path

--- src/routes/adminVoucherRealstageRoutes.js
+++ src/routes/adminVoucherRealstageRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const db = require('../db/neonClient');
 
 // GET /api/admin/voucher-realstage
 // Ambil semua data voucher dengan filter pencarian dan pagination opsional
--- src/routes/voucherRealstageRoutes.js
+++ src/routes/voucherRealstageRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const db = require('../db/neonClient');
 
 // GET /api/voucher-realstage
 // Mengambil data voucher. Bisa filter berdasarkan pencarian.
